1383cea3aa7cbcf97b63ce6a5a3bfc40d3757a79
Savlatjon Khuseynov <savlat1133@gmail.com>
2024-05-28 19:09:38 +0100

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Savlatjon" };
+  const loggedIn = {
+    firstName: "Savlatjon",
+    lastName: "Khuseynov",
+    email: "const@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -22,7 +27,13 @@ const Home = () => {
             totalCurrentBalance={1250.67}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1201 }, { currentBalance: 4322 }]}
+      />
     </section>
   );
 };
